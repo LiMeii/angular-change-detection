c621729a3f66150990b951608c38a7c6b5945827
LiMeii <mei90s.lee@gmail.com>
2019-07-05 17:45:17 +0800

    add rxjs demo code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,28 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
 const routes: Routes = [
   {
-    path: '',
-    pathMatch: 'full',
-    redirectTo: 'unidirectionalDataFlow'
+    path: "",
+    pathMatch: "full",
+    redirectTo: "unidirectionalDataFlow"
   },
   {
-    path: 'unidirectionalDataFlow',
-    loadChildren: './modules/unidirectional-data-flow/unidirectional-data-flow.module#UnidirectionalDataFlowModule',
+    path: "unidirectionalDataFlow",
+    loadChildren: "./modules/unidirectional-data-flow/unidirectional-data-flow.module#UnidirectionalDataFlowModule",
     data: { preload: true }
   },
   {
-    path: 'changedetection',
-    loadChildren: './modules/change-detection/change-detection.module#ChangeDetectionModule'
+    path: "changedetection",
+    loadChildren: "./modules/change-detection/change-detection.module#ChangeDetectionModule"
   },
   {
-    path: 'onpushngdocheck',
-    loadChildren: './modules/onpush-ngdocheck/onpush-ngdocheck,module#OnpushNgdocheckModule'
+    path: "onpushngdocheck",
+    loadChildren: "./modules/onpush-ngdocheck/onpush-ngdocheck.module#OnpushNgdocheckModule"
+  },
+  {
+    path: "rxjslearning",
+    loadChildren: "./modules/rxjs/rxjs-learning.module#RxjsLearningModule"
   }
 ];
 
